d1489aceb5157d29e0094a93fcf3bab4d5e22395
capybara120404 <D.L.Borodin@outlook.com>
2024-11-14 22:21:48 +0300

    A model describing the TV series has been created.

---
+++ common/models/series.go
@@ -0,0 +1,18 @@
+package models
+
+type Series struct {
+	ID              uint    `json:"id"`
+	Title           string  `json:"title"`
+	Link            string  `json:"link"`
+	IMDB            float32 `json:"imdb"`
+	StartYear       int     `json:"start_year"`
+	EndYear         *int    `json:"end_year,omitempty"`
+	PosterLink      string  `json:"poster_link"`
+	Country         string  `json:"country"`
+	NumberOfEpisode uint    `json:"number_of_episode"`
+	EpisodeDuration uint    `json:"episode_duration"`
+}
+
+func (s *Series) CalculateTotalDuration() uint {
+	return s.NumberOfEpisode * s.EpisodeDuration
+}
