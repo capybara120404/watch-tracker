4e1614f43f1d83e841f4bca62aa26df7d69e58e4
capybara120404 <D.L.Borodin@outlook.com>
2024-11-14 22:01:12 +0300

    The logic for creating a custom logger has been changed.

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func CreateLogger(logFile, nameOfLogger string) (*log.Logger, *os.File, error) {
-	err := os.MkdirAll("common/logs", 0755)
+	logFilePath := filepath.Join("common", "logs", logFile)
+
+	err := os.MkdirAll(filepath.Dir(logFilePath), 0755)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to create directories for log file %s: %v", logFile, err)
+		return nil, nil, fmt.Errorf("failed to create directories for log file %s: %v", logFilePath, err)
 	}
 
-	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to open log file %s: %w", logFile, err)
+		return nil, nil, fmt.Errorf("failed to open log file %s: %w", logFilePath, err)
 	}
 
 	logger := log.New(file, nameOfLogger, log.LstdFlags|log.Lshortfile)
