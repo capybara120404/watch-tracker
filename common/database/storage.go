75b893db492cf08a1d65094704bc4c39fda8672d
capybara120404 <D.L.Borodin@outlook.com>
2024-11-14 22:48:13 +0300

    Added the ability to connect or create a database.

---
+++ common/database/storage.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Storage struct {
+	DB *sql.DB
+}
+
+func Open(connectionString string) (*Storage, error) {
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("error opening PostgreSQL database: %v", err)
+	}
+	log.Printf("PostgreSQL Database connected successfully.")
+
+	err = createPostgresDatabase(db)
+	if err != nil {
+		return nil, fmt.Errorf("error creating PostgreSQL database: %v", err)
+	}
+
+	return &Storage{DB: db}, nil
+}
+
+func (c *Storage) Close() error {
+	if err := c.DB.Close(); err != nil {
+		return fmt.Errorf("error closing database: %v", err)
+	}
+	return nil
+}
+
+func createPostgresDatabase(db *sql.DB) error {
+	createTables := `
+	CREATE TABLE IF NOT EXISTS series (
+		id SERIAL PRIMARY KEY,
+		title TEXT NOT NULL,
+		link TEXT NOT NULL,
+		imdb FLOAT,
+		start_year INT,
+		end_year INT,
+		poster TEXT,
+		country TEXT,
+		number_of_episode INT,
+		episode_duration INT
+	);
+	`
+
+	_, err := db.Exec(createTables)
+	if err != nil {
+		return fmt.Errorf("query execution error: %v", err)
+	}
+
+	return nil
+}
