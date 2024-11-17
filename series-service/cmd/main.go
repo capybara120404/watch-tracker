3cd6f6a405db9288c23a3cde0e6bc17885c046ae
capybara120404 <D.L.Borodin@outlook.com>
2024-11-18 00:46:15 +0300

    Added a file to start the service.

---
+++ series-service/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/capybara120404/watch-tracker/common/config"
+	"github.com/capybara120404/watch-tracker/common/database"
+	"github.com/capybara120404/watch-tracker/common/utils"
+	"github.com/capybara120404/watch-tracker/series-service/internal/handler"
+	"github.com/capybara120404/watch-tracker/series-service/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config, err := config.NewConfig("./common/config/config.env")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	logger, file, err := utils.CreateLogger(config.LogFile, config.NameOFLogger)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	storage, err := database.Open(config.ConnectionString)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	repository := repository.NewSeriesRepository(storage)
+	handler := handler.NewSeriesHandler(logger, repository)
+
+	router := gin.Default()
+
+	seriesGroup := router.Group("/series")
+	{
+		seriesGroup.DELETE("/:id", handler.DeleteSeriesByIDHandler)
+		seriesGroup.GET("", handler.GetAllSeriesHandler)
+		seriesGroup.GET("/:id", handler.GetSeriesByIdHandler)
+		seriesGroup.POST("", handler.AddSeriesHandler)
+	}
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.Run(":8000")
+}
